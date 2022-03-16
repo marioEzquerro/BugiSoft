2c7c26c272e07affbf198f30c4f78145ee091dd6
Rallensit0 <calvarezd02@larioja.edu-es>
2022-03-16 16:49:45 +0100

    Pagina_Noticias

--- WEB/src/app/pages/home/noticias-page/noticias-page.component.ts
+++ WEB/src/app/pages/home/noticias-page/noticias-page.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Noticia } from 'src/app/models/noticia.model';
+import { NoticiaService } from 'src/app/services/noticia.service';
 
 @Component({
   selector: 'app-noticias-page',
   templateUrl: './noticias-page.component.html',
-  styleUrls: ['./noticias-page.component.css']
+  styleUrls: ['./noticias-page.component.css'],
 })
 export class NoticiasPageComponent implements OnInit {
+  noticias: Noticia[] | null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private _noticiaService: NoticiaService) {
+    this.noticias = null;
   }
 
+  ngOnInit(): void {
+    this._noticiaService.getNoticiaData().subscribe((x) => (this.noticias = x));
+  }
 }
