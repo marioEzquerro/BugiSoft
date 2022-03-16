2c8dd4beea9e459da6ce495b4fa3851b05a6f056
Rallensit0 <calvarezd02@larioja.edu-es>
2022-03-16 17:37:51 +0100

    UltimaNoticia

--- WEB/src/app/services/noticia.service.ts
+++ WEB/src/app/services/noticia.service.ts
@@ -7,29 +7,34 @@ import { Noticia } from '../models/noticia.model';
 @Injectable()
 export class NoticiaService {
   constructor(private http: HttpClient) {}
-  getNoticiaData() : Observable<Noticia[]> {
+  getNoticiaData(): Observable<Noticia[]> {
     return this.http.get<Noticia[]>(environment.API_URL + 'noticias');
   }
 
-  postNoticiaData(body : any) : Noticia {
-    let bodyData =new Noticia();
-    bodyData.nombre=body.nom;
-    bodyData.descripcion_corta=body.desc_c;
-    bodyData.descripcion=body.desc_l;
-    bodyData.img=body.img;
-    bodyData.fecha= new Date();
+  getLastNoticiaData(): Observable<Noticia> {
+    return this.http.get<Noticia>(environment.API_URL + 'noticias/last');
+  }
 
-    let result =new Noticia();
-    this.http.post<Noticia>(environment.API_URL + 'noticias',bodyData)
-    .subscribe(
-      (response) => {
-        console.log('response received')
-        result = response;
-      },
-      (error) => {
-        console.error('error caught in component')
-      }
-    )
+  postNoticiaData(body: any): Noticia {
+    let bodyData = new Noticia();
+    bodyData.nombre = body.nom;
+    bodyData.descripcion_corta = body.desc_c;
+    bodyData.descripcion = body.desc_l;
+    bodyData.img = body.img;
+    bodyData.fecha = new Date();
+
+    let result = new Noticia();
+    this.http
+      .post<Noticia>(environment.API_URL + 'noticias', bodyData)
+      .subscribe(
+        (response) => {
+          console.log('response received');
+          result = response;
+        },
+        (error) => {
+          console.error('error caught in component');
+        }
+      );
     return result;
   }
 }
