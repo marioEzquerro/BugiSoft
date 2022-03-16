a340806c75843a59d6eb4042441fc4f511faa6f0
marioEzquerro <marioezquerroruiz@gmail.com>
2022-03-16 18:10:47 +0100

    Layout biblioteca done

--- WEB/src/app/services/juego.service.ts
+++ WEB/src/app/services/juego.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Juego } from '../models/juego.model'; 
+import { Juego } from '../models/juego.model';
 
 @Injectable()
 export class JuegoService {
@@ -10,4 +10,6 @@ export class JuegoService {
   getJuegoData() : Observable<Juego[]> {
     return this.http.get<Juego[]>(environment.API_URL + 'juegos');
   }
+
+
 }
