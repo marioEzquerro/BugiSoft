64afd8cd3e76d8773f2737624223da7ca11ec3d4
Rallensit0 <calvarezd02@larioja.edu-es>
2022-03-16 20:10:18 +0100

    ArregladoObtenerId

--- WEB/src/app/components/forbidden-url-component/forbidden-url-component.component.ts
+++ WEB/src/app/components/forbidden-url-component/forbidden-url-component.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-forbidden-url-component',
   templateUrl: './forbidden-url-component.component.html',
-  styleUrls: ['./forbidden-url-component.component.css']
+  styleUrls: ['./forbidden-url-component.component.css'],
 })
 export class ForbiddenUrlComponentComponent implements OnInit {
+  constructor(private _location: Location) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  backClicked() {
+    this._location.back();
   }
 
+  ngOnInit(): void {}
 }
