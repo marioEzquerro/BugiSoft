bb59dce6035a35fffe40c512b8d489be4581397f
Rallensit0 <calvarezd02@larioja.edu-es>
2022-03-16 18:22:30 +0100

    juegoService

--- WEB/src/app/services/juego.service.ts
+++ WEB/src/app/services/juego.service.ts
@@ -7,34 +7,29 @@ import { Juego } from '../models/juego.model';
 @Injectable()
 export class JuegoService {
   constructor(private http: HttpClient) {}
-  getJuegoData() : Observable<Juego[]> {
+  getJuegoData(): Observable<Juego[]> {
     return this.http.get<Juego[]>(environment.API_URL + 'juegos');
   }
 
-<<<<<<< HEAD
-=======
-  postJuegoData(body : any) : Juego {
-    let bodyData =new Juego();
+  postJuegoData(body: any): Juego {
+    let bodyData = new Juego();
     bodyData.nombre = body.nom;
     bodyData.descripcion = body.desc;
-    bodyData.fecha_lanzamiento =  new Date(body.fecha);
+    bodyData.fecha_lanzamiento = new Date(body.fecha);
     bodyData.genero = body.genero;
     bodyData.plataforma = body.plat;
     bodyData.imagen = body.img;
 
-    let result =new Juego();
-    this.http.post<Juego>(environment.API_URL + 'juegos',bodyData)
-    .subscribe(
+    let result = new Juego();
+    this.http.post<Juego>(environment.API_URL + 'juegos', bodyData).subscribe(
       (response) => {
-        console.log('response received')
+        console.log('response received');
         result = response;
       },
       (error) => {
-        console.error('error caught in component')
+        console.error('error caught in component');
       }
-    )
+    );
     return result;
   }
->>>>>>> 9b1fae1ed23cc01541e71343955a0f9a421e32f7
-
 }
